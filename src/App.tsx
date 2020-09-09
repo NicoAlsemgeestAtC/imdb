541178730eea5fc894de9630af6304c50eeeb152
Nico Alsemgeest <nalsemgeest@conclusion.nl>
2020-09-09 15:45:33 +0200

    Fix warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import AboutPage from "./Pages/AboutPage";
 import HomePage from "./Pages/HomePage";
 import DiscoverMoviesPage from "./Pages/DiscoverMoviesPage";
